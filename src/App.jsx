83ee9d5ad8c7bf753794a98c4b55ae826f446519
sohan-99 <sohan75632@gmail.com>
2024-10-24 15:46:57 +0600

    adding Article Detail Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
 
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home/HomePage';
-
+import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage';
+// import ArticleDetailPage from './pages/home/ArticleDetail/ArticleDetailPage'
 function App() {
   return (
     <div className="App font-opensans w-full">
-    <HomePage />
+    <Routes>
+  <Route path="/" element={<HomePage />} />
+  <Route path="/blog/:id" element={<ArticleDetailPage />} />
+</Routes>
+
   </div>
   );
 }
