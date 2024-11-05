217f6436ad586632a0ed97856bc83b7261c2f292
sohan-99 <sohan75632@gmail.com>
2024-11-06 00:49:28 +0600

    register page

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import HomePage from './pages/home/HomePage';
 import ArticleDetailPage from './pages/articleDetail/ArticleDetailPage';
+import RegisterPage from './pages/register/RegisterPage';
 function App() {
   return (
     <div className="App font-opensans w-full">
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/blog/:id" element={<ArticleDetailPage />} />
+        <Route path="/register" element={<RegisterPage />} />
       </Routes>
     </div>
   );
